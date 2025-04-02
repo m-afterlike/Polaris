7f9b848448d03b3e33544c2674bf59729b486225
GitHub Action <action@github.com>
2025-04-02 00:18:25 +0000

    Initialize template repository proper
    Set package name to com.github.m-afterlike.polaris
    Set project name to Polaris
    Set modid to polaris

--- src/main/java/com/github/m-afterlike/polaris/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/m-afterlike/polaris/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.m-afterlike.polaris.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/kotlin/com/github/m-afterlike/polaris/mixin/MixinGuiMainMenu.java
+++ src/main/kotlin/com/github/m-afterlike/polaris/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.m-afterlike.polaris.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
